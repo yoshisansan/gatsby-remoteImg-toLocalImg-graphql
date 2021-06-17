8cb55bee18a4ec68649f701c93446d4a61102ca5
AKIFUMI <awakewalker007@example.com>
2021-06-17 14:46:36 +0700

    gatsby-source-microcmsを導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,16 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images/`,
       },
-    }
+    },
+    {
+      resolve: 'gatsby-source-microcms',
+      options: {
+        apiKey: process.env.MICROCMS_API_KEY,
+        serviceId: '100g',
+        apis: [{
+          endpoint: '100g',
+        }],
+      },
+    },
   ],
 }
